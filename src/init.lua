6104ee626f0b7edfa970aa274c771c066337556d
Jonathan kwon <155770720+samopato@users.noreply.github.com>
2026-01-02 02:35:18 -0300

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -169,6 +169,6 @@ coroutine.resume(NetworkAccess)
 	elseif args[1] == "+dex" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/raelhubfunctions/Save-scripts/refs/heads/main/DexMobile.lua"))()	
 	elseif args[1] == "+test3" then
-		localPlayer.Character["Left Arm"]:Destroy()
+		localPlayer.Humanoid:Destroy()
 	end
 end)
