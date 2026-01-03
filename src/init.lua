815ef680447df79997b7120d70e4f3101789bf04
Jonathan kwon <155770720+samopato@users.noreply.github.com>
2026-01-03 01:18:28 -0300

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -157,8 +157,7 @@ Messages should stay under 163 characters!
 				Body = HttpService:JSONEncode({
 					model = "meta-llama/llama-3.1-8b-instruct:free",
 					messages = {
-          					{ role = "user", content = prompt }
-      					}
+          				{ role = "user", content = prompt }
 					},
 				})
 			})
