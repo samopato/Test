869bcee59e3895f3c98fd1577260e7ae75e406a1
Jonathan kwon <155770720+samopato@users.noreply.github.com>
2026-01-05 21:18:20 -0300

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -63,7 +63,7 @@ local function bypass(text)
 	}
 
 	local translated = string.gsub(text, "@%w+", dictionary)
-	local bypassed = string.gsub(translated, ".", "%0\xD8\x8D")
+	local bypassed = string.gsub(translated, ".", "%0\xD8\x8D ")
 
 	return bypassed
 end
