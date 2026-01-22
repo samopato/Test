74d98738f45ba8b7d1d9edf09196a6f6c5d8d85b
Jonathan kwon <155770720+samopato@users.noreply.github.com>
2026-01-22 00:57:32 -0300

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -1578,10 +1578,10 @@ local function hexToAnsi(hexColor)
 		return "[32m" -- Green
 	elseif r > 150 and g > 150 and b < 100 then
 		return "[33m" -- Yellow
-	elseif b > r + 50 and b > g + 50 then
-		return "[34m" -- Blue
 	elseif r + 10 > g and r > b and g < 200 then
 		return "[35m" -- Magenta
+	elseif b > r + 50 and b > g + 50 then
+		return "[34m" -- Blue
 	elseif g > 150 and b > 150 and r < 150 then
 		return "[36m" -- Cyan
 	elseif r > 200 and g > 200 and b > 200 then
