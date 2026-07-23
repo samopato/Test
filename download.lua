33b8bcb274483f1f3359bd04d022c55931526fe8
Jonathan kwon <155770720+samopato@users.noreply.github.com>
2026-07-23 02:56:13 -0300

    Update download.lua

--- download.lua
+++ download.lua
@@ -47,7 +47,7 @@ local function downloadFolder(url, localPath)
 			if not isfolder(itemLocalPath) then makefolder(itemLocalPath) end
 			downloadFolder(item.url, itemLocalPath)
 		elseif item.type == "file" and item.download_url then
-			TextChatService.TextChannels.RBXGeneral:SendAsync(`VEX: Downloading {item.name}...`)
+			log(`VEX: Downloading {item.name}...`)
 			local content = request(item.download_url)
 			if content then
 				writefile(itemLocalPath, content)
